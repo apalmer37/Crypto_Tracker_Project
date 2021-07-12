b6263ea0579a0a06df84be379879f6d7428af41c
Wes Burns <burnsw5@gmail.com>
2021-07-12 19:38:18 -0400

    Added coins

--- app.js
+++ app.js
@@ -5,7 +5,29 @@ var apikey = {
 request('GET','https://enigmatic-bayou-32442.herokuapp.com/'+'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=' + apikey.key)
 .then((r1) => {
     var x1 = JSON.parse(r1.target.responseText);
-    document.querySelector("#bitcoin").textContent = x1.data.max_supply;
+    // Bitcoin //
+    document.querySelector("#bitcoinprice").textContent = x1.data[0].quote.USD.price;
+    document.querySelector("#bitcoinday").textContent = x1.data[0].quote.USD.percent_change_24h;
+    document.querySelector("#bitcoinvolume").textContent = x1.data[0].quote.USD.volume_24h;
+    // Dogecoin //
+    document.querySelector("#bitcoinprice").textContent = x1.data[6].quote.USD.price;
+    document.querySelector("#bitcoinday").textContent = x1.data[6].quote.USD.percent_change_24h;
+    document.querySelector("#bitcoinvolume").textContent = x1.data[6].quote.USD.volume_24h;
+    // Litecoin //
+    document.querySelector("#bitcoinprice").textContent = x1.data[12].quote.USD.price;
+    document.querySelector("#bitcoinday").textContent = x1.data[12].quote.USD.percent_change_24h;
+    document.querySelector("#bitcoinvolume").textContent = x1.data[12].quote.USD.volume_24h;
+    // SushiSwap //
+    document.querySelector("#bitcoinprice").textContent = x1.data[72].quote.USD.price;
+    document.querySelector("#bitcoinday").textContent = x1.data[72].quote.USD.percent_change_24h;
+    document.querySelector("#bitcoinvolume").textContent = x1.data[72].quote.USD.volume_24h;
+    // Ethereum //
+    document.querySelector("#bitcoinprice").textContent = x1.data[1].quote.USD.price;
+    document.querySelector("#bitcoinday").textContent = x1.data[1].quote.USD.percent_change_24h;
+    document.querySelector("#bitcoinvolume").textContent = x1.data[1].quote.USD.volume_24h;
+    
+
+   
     console.log(x1);
 }).catch(err => {
     console.log(err);
