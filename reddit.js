5aad1c53a4fe34edfe8a2c8033ee96b74da4399f
Alex Palmer <alexpalmer37@gmail.com>
2021-07-18 13:36:36 -0400

    Update to CSS

--- reddit.js
+++ reddit.js
@@ -1,5 +1,6 @@
 const domain = "https://www.reddit.com";
 var list = document.querySelector('#listPosts');
+var recentList = [];
 
 console.log(list);
 
@@ -26,4 +27,48 @@ $().ready(()=>{
       list.appendChild(listItem);
     });
   });
+});
+
+initClickedLink();
+
+function renderRecentEl(){
+  $("#recentList").empty();
+  $("#clickedLink").val("");
+
+  for (i=0; i<recentList.length; i++) {
+    var a = $("<a>");
+    a.addClass("list-group-item list-group-item-action list-group-item-primary recentClick");
+    a.attr("data-input", recentList[i])
+    a.text(recentList[i]);
+    $("#recentList").prepend(a);
+  }
+}
+
+function initClickedLink(){
+  var storedRecentEl = JSON.parse(localStorage.getItem("recentEl"));
+  if (storedRecentEl !== null) {
+    recentList = storedRecentEl;
+  }
+  renderRecentEl();
+}
+
+function storeRecentClickList(){
+  localStorage.setItem("recentEl", JSON.stringify(recentList));
+}
+
+function storeLastClick(){
+  localStorage.setItem("lastClick", JSON.stringify(list));
+}
+
+$(document).on("click", "redditPermaLink", function(event){
+  event.preventDefault();
+
+  recentList = $("clickedLink").val().trim();
+  if(recentList >= 5) {
+    recentList.shift();
+    recentList.push(list);
+  }
+  renderRecentEl();
+  storeRecentClickList();
+  storeLastClick();
 });
